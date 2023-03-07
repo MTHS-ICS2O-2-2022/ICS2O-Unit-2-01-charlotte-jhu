98aa15d21ac37d6c8aabd1b78102d96eac574c0c
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-07 18:22:44 +0000

    added extra

---
+++ go_app/main.go
@@ -0,0 +1,15 @@
+// Created by: Charlotte Jhu
+// Created on: March 2023
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function displays, "Hello, World!"
+	fmt.Println("Hello, World!")
+
+  fmt.Println("\nDone.")
+}
